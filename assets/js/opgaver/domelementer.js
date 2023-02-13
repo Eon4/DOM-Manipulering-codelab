f5d8e07cf155af18a9aa15c1223c7aa0b712287d
Eon4 <ladyforest85@gmail.com>
2023-02-13 11:27:53 +0100

    Update domelementer.js

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -32,7 +32,7 @@ Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
 - et h2 med teksten: Opgave 2.1 løsning
 - et p med teksten: Jeg har løst opgave 2.1
 */
-const myLiveList = document.getElementById('opgaveTwo');
+const something = document.getElementById('opgaveTwo');
 
 let myHeadline = document.createElement('h2');
 myHeadline.innerHTML = 'Opgave 2.1 løsning';
@@ -47,6 +47,7 @@ console.log(myParagraph);
 /* opgave 3.1
 Du skal finde alle elementer med klassen purpleFigures og console logge resultatet
 */
+const purpleCircles = document.getElementsByClassName("purpleFigures myFigure")
 
 console.log(document.getElementsByClassName('purpleFigures'));
 
@@ -56,19 +57,25 @@ Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til
 til et array hvor du kan bruge map funktionen til at ændre baggrundsfarven til rød.
 array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
 */
+// console.log(Array.from('purpleFigures'));
+// let myArray = Array.from('purpleFigures');
+// myArray.map((myElement) => {
+//     myElement.style.backgroundColor = "red";
+// });
 
-// din kode her
-
+const myArray = Array.from(purpleCircles)
+console.log(myArray)
+myArray.map((myArray) => {
+    myArray.style.backgroundColor = "red";
+    myArray.children[0].innerHTML = Text = "RED";
+});
 
 /* opgave 3.3
 Brug myLiveList til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
 myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 */
 
-
-// din kode her
-
-
+//SE OPGAVE 3.2
 
 /* opgave 4
 opret DOM elementer i elementet med id opgaveFour, udfra data objectet myData.
